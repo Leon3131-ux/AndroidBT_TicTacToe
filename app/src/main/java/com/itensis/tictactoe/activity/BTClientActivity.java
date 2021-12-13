56ed0502254f61dc43b8c13d086b998aace51947
leon3131-ux <knud.del@hotmail.com>
2021-12-13 16:38:55 +0100

    refactoring for BTClientActivity

--- app/src/main/java/com/itensis/tictactoe/activity/BTClientActivity.java
+++ app/src/main/java/com/itensis/tictactoe/activity/BTClientActivity.java
@@ -1,6 +1,5 @@
 package com.itensis.tictactoe.activity;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
@@ -9,9 +8,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.provider.Settings;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -26,9 +25,6 @@ import java.util.stream.Collectors;
 public class BTClientActivity extends AppCompatActivity {
 
     private final BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
-    private final static int REQUEST_ENABLE_BT = 1;
-    private final static int REQUEST_ENABLE_DISCOVERABILITY = 2;
-    private final static int REQUEST_ENABLE_LOCATION = 3;
     private TextView statusText;
     private ListView devicesList;
     private final List<BluetoothDevice> foundDevices = new ArrayList<>();
@@ -40,67 +36,59 @@ public class BTClientActivity extends AppCompatActivity {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 if(!foundDevices.contains(device)){
                     foundDevices.add(device);
-                    List<String> names = foundDevices
-                            .stream()
-                            .map(BluetoothDevice::getName)
-                            .filter(Objects::nonNull)
-                            .collect(Collectors.toList());
-                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                            BTClientActivity.this,
-                            android.R.layout.simple_expandable_list_item_1,
-                            names
-                    );
-                    devicesList.setAdapter(adapter);
+                    updateFoundDevicesList();
                 }
             }
         }
     };
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_client);
+
         statusText = findViewById(R.id.clientStatusText);
         devicesList = findViewById(R.id.deviceListView);
+
+        registerDeviceFoundReceiver();
+        registerDeviceClicked();
+
+        discoverDevices();
+    }
+
+    private void discoverDevices(){
+        btAdapter.startDiscovery();
+    }
+
+
+    private void updateFoundDevicesList(){
+        List<String> names = foundDevices
+                .stream()
+                .map(BluetoothDevice::getName)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                BTClientActivity.this,
+                android.R.layout.simple_expandable_list_item_1,
+                names
+        );
+        devicesList.setAdapter(adapter);
+    }
+
+    private void registerDeviceFoundReceiver(){
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         registerReceiver(receiver, filter);
-        enableLocation();
-        setUp();
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_ENABLE_BT) {
-            if (resultCode == RESULT_OK) {
-                statusText.setText("Bluetooth enabled");
-                if(btAdapter.startDiscovery()){
-                    statusText.setText("Discovering");
-                }
+    private void registerDeviceClicked(){
+        devicesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                BluetoothDevice device = foundDevices.get(position);
+
+                //TODO handle connect code
             }
-        }
-    }
-
-    private void setUp(){
-        if(btAdapter != null){
-            if(!btAdapter.isEnabled()){
-                Intent enableBt = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enableBt, REQUEST_ENABLE_BT);
-            }else {
-                if(btAdapter.startDiscovery()){
-                    statusText.setText("Discovering");
-                }
-            }
-        }
-    }
-
-    private void enableLocation(){
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        boolean isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        if (!isGpsEnabled) {
-            startActivityForResult(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS), REQUEST_ENABLE_LOCATION);
-        }
+        });
     }
 
 }
--- app/src/main/java/com/itensis/tictactoe/activity/BTHomeActivity.java
+++ app/src/main/java/com/itensis/tictactoe/activity/BTHomeActivity.java
@@ -1,16 +1,26 @@
 package com.itensis.tictactoe.activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
 import com.itensis.tictactoe.R;
 
 public class BTHomeActivity extends AppCompatActivity {
+
+    private boolean hasRequiredPermissions = false;
+    private boolean btEnabled = false;
+    private static final int REQUEST_ENABLE_LOCATION = 1;
+    private static final int REQUEST_ENABLE_BLUETOOTH = 2;
+    private final BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +29,53 @@ public class BTHomeActivity extends AppCompatActivity {
         ActivityCompat.requestPermissions(
                 this,
                 new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                1);
+                REQUEST_ENABLE_LOCATION);
+        enableBluetooth();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_ENABLE_LOCATION){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                hasRequiredPermissions = true;
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_ENABLE_BLUETOOTH){
+            if(resultCode == RESULT_OK){
+                btEnabled = true;
+            }
+        }
+    }
+
+    private void enableBluetooth(){
+        if(btAdapter != null){
+            if(!btAdapter.isEnabled()){
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BLUETOOTH);
+
+            }
+            btEnabled = true;
+        }
     }
 
     public void hostGame(View v){
-        Intent hostIntent = new Intent(BTHomeActivity.this, BTHostActivity.class);
-        startActivity(hostIntent);
+        if(hasRequiredPermissions && btEnabled){
+            Intent hostIntent = new Intent(BTHomeActivity.this, BTHostActivity.class);
+            startActivity(hostIntent);
+        }
     }
 
     public void joinGame(View v){
-        Intent clientIntent = new Intent(BTHomeActivity.this, BTClientActivity.class);
-        startActivity(clientIntent);
+        if(hasRequiredPermissions && btEnabled){
+            Intent clientIntent = new Intent(BTHomeActivity.this, BTClientActivity.class);
+            startActivity(clientIntent);
+        }
     }
 
 }
